681bad63a706f4d732936c0f3222e9958f63ab1e
Walter Tan <waltertan12@gmail.com>
2015-09-17 13:04:24 -0700

    Annotate models and add validations for Band/Album/Track

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  band_id    :integer          not null
+#  live       :boolean          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Album < ActiveRecord::Base
+  validates  :title, :band_id, :live, presence: true
+  belongs_to :band
+  has_many   :tracks, dependent: :destroy
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  album_id   :integer          not null
+#  bonus      :boolean          not null
+#  lyrics     :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Track < ActiveRecord::Base
+  validates  :name, :album_id, :bonus, :lyrics, presence: true
+  belongs_to :album
 end
